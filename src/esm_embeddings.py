2896f133459e52582077dc3cd2b9adff2b4e273d
Kornelius20-20 <weeraman97@gmail.com>
2024-09-23 21:22:16 -0600

    adding transformers to the requirements

--- src/esm_embeddings.py
+++ src/esm_embeddings.py
@@ -4,7 +4,9 @@ import torch
 tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t6_8M_UR50D")
 model = EsmModel.from_pretrained("facebook/esm2_t6_8M_UR50D")
 
-inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+inputs = tokenizer("MQIFVKTLTGKTITLEVEPSTIENVKAKIQDKEGIPPDQQRLIFAGKQLEDGRTLSDYNIQKESTLHLVLRLRGG", return_tensors="pt")
 outputs = model(**inputs)
 
 last_hidden_states = outputs.last_hidden_state
+
+print(last_hidden_states)
