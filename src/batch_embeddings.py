f64008627352f658fca4a99e6bf22d38aea2a34f
Kornelius20-20 <weeraman97@gmail.com>
2024-09-24 15:26:58 -0600

    fixing variabte naming bug

--- src/batch_embeddings.py
+++ src/batch_embeddings.py
@@ -2,11 +2,12 @@ import torch,os, gzip
 
 from esm_embeddings import generate_embeddings
 
-fastafolder = os.listdir("temp/fastas/")
+tempfolder = "temp/fastas/"
+fastafolder = os.listdir(tempfolder)
 
 for file in fastafolder:
     # Create a BytesIO buffer for the gzip file and extract the content
-    with gzip.open(os.path.join(fastafolder,file),'rt') as handle:
+    with gzip.open(os.path.join(tempfolder,file),'rt') as handle:
         data = handle.read()
     
     print(data); quit()
