5f481351681e7540145e1d9be264494ca167a047
Ganeshiny Sridharan <ganeshinywork@gmail.com>
2024-09-24 23:09:46 +0530

    workflow and graph data code rough outline

--- src/generate_graph.py
+++ src/generate_graph.py
@@ -1,4 +1,5 @@
 from torch_geometric.data import HeteroData
+import torch 
 
 '''
 Inputs:
@@ -8,3 +9,34 @@ Drug class embeddings : Shape [num_drug_classes, embedding_dim]
 Edge Indices: [2, num_edges] #This we have to create a function that makes COO matrices, takes the 
 
 '''
+
+
+'''
+===============Dummy Data=====================
+'''
+num_proteins = 12
+embedding_dim =1028
+num_drug_classes = 10
+
+protein_embeddings = torch.randn(num_proteins, embedding_dim)
+drug_class_embeddings = torch.randn(num_drug_classes, embedding_dim) #binary matrix
+edge_index = torch.tensor([[0, 1, 2, 3],  # Protein indices
+                           [0, 2, 1, 3]])
+
+'''
+===============Dummy Data=====================
+'''
+
+data = HeteroData()
+
+data['protein'].x = protein_embeddings  # Shape [num_proteins, embedding_dim]
+
+data['drug_class'].x = drug_class_embeddings  # Shape [num_drug_classes, embedding_dim]
+
+data['protein', 'interacts_with', 'drug_class'].edge_index = edge_index  # Shape [2, num_edges]
+
+
+print(data['protein'].x)
+print(data['drug_class'].x)
+print(data['protein', 'interacts_with', 'drug_class'].edge_index)
+print(data)
