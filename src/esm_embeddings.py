fc356b4baf8a682485a24e5c74b401088db21bca
Kornelius20-20 <weeraman97@gmail.com>
2024-09-24 08:09:53 -0600

    Creating the generate embeddings function

--- src/esm_embeddings.py
+++ src/esm_embeddings.py
@@ -1,12 +1,34 @@
-from transformers import AutoTokenizer, EsmModel
 import torch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t6_8M_UR50D")
-model = EsmModel.from_pretrained("facebook/esm2_t6_8M_UR50D")
+def generate_embeddings(
+        sequences, # list of tuples of the form ('header','sequence')
+        device=device, # will automatically use CUDA if its available else will fall back on cpu
+):
 
-inputs = tokenizer("MQIFVKTLTGKTITLEVEPSTIENVKAKIQDKEGIPPDQQRLIFAGKQLEDGRTLSDYNIQKESTLHLVLRLRGG", return_tensors="pt")
-outputs = model(**inputs)
+    model, alphabet = torch.hub.load("facebookresearch/esm:main", "esm2_t33_650M_UR50D")
+    batch_converter = alphabet.get_batch_converter()
+    model.eval()
+    model.to(device)
 
-last_hidden_states = outputs.last_hidden_state
+    # Generating data batches
+    batch_labels, batch_strs, batch_tokens = batch_converter(sequences)
+    with torch.no_grad():
+        batch_tokens = batch_tokens.to(device)
+        results = model(batch_tokens, repr_layers=[33], return_contacts=True)
+    token_representations = results["representations"][33]
 
-print(last_hidden_states)
+    # Generating sequence representating via averaging token representations
+    sequence_representations = []
+    for i, (_, seq) in enumerate(sequences):
+        sequence_representations.append(token_representations[i, 1 : len(seq) + 1].mean(0))
+    
+    return [(batch_labels[i],rep) for i,rep in enumerate(sequence_representations)] # Embedding dim is 1280
+
+if __name__=="__main__":
+    data = [
+    ("protein1", "MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG"),
+    ("protein2", "KALTARQQEVFDLIRDHISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE"),
+    ]
+
+    print(generate_embeddings(data,device)) 
