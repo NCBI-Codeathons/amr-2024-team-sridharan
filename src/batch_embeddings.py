4a7f2f4627eb62b1385d14d2ba3a3f6540158e54
Kornelius20-20 <weeraman97@gmail.com>
2024-09-24 15:38:35 -0600

    creating partial dumps every 100000

--- src/batch_embeddings.py
+++ src/batch_embeddings.py
@@ -1,13 +1,25 @@
-import torch,os, gzip
+import torch,os, gzip,pickle
 
 from esm_embeddings import generate_embeddings
 
 tempfolder = "temp/fastas/"
 fastafolder = os.listdir(tempfolder)
+batch_size = 100
 
-for file in fastafolder:
+sequences = []
+embeddings = []
+
+for i,file in enumerate(fastafolder):
     # Create a BytesIO buffer for the gzip file and extract the content
     with gzip.open(os.path.join(tempfolder,file),'rt') as handle:
         data = ''.join(handle.read().split('\n')[1:])
+    sequences.append((file,data))
     
-    print(data); quit()
+    if i%batch_size==0:
+        embeddings += generate_embeddings(sequences)
+        sequences = []
+    
+    if i%(batch_size*1000)==0:
+        with open(f"temp/fastas/embeddings/{i},pickle", 'wb') as handle:
+            pickle.dump(embeddings,handle)
+        embeddings = []
