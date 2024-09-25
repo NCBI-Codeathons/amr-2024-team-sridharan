67a756dba0f5305685e595d61a3f9b02fe905116
Kornelius20-20 <weeraman97@gmail.com>
2024-09-25 11:37:50 -0600

    changing embedding dim

--- src/generate_graph.py
+++ src/generate_graph.py
@@ -16,7 +16,7 @@ Edge Indices: [2, num_edges] #This we have to create a function that makes COO m
 ===============Dummy Data=====================
 '''
 num_proteins = 12
-embedding_dim =1028
+embedding_dim =480
 num_drug_classes = 10
 
 protein_embeddings = torch.randn(num_proteins, embedding_dim)
