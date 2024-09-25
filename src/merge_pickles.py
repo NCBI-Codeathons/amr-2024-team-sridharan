c53c569632f062fee6f651ffb6ecf50f35ab3bf5
Kornelius20-20 <weeraman97@gmail.com>
2024-09-25 12:58:02 -0600

    adding pickle merge code

---
+++ src/merge_pickles.py
@@ -0,0 +1,25 @@
+import os,pickle
+import numpy as np
+
+pickledir = "/temp/embeddings"
+
+files = os.listdir(pickledir)
+
+data = []
+for file in files:
+
+    with open(os.path.join(pickledir,file),'rb') as handle:
+        data += handle.read()
+
+embeddings = [i[1] for i in data]
+accessions = [i[0] for i in data]
+
+embeddings = np.array(embeddings)
+
+dir_above_pickledir = '/'.join(pickledir.split('/')[:-1])
+
+with open(os.path.join(dir_above_pickledir,"all_embeddings.pickle"),'wb') as handle:
+    pickle.dump(embeddings,handle)
+
+with open(os.path.join(dir_above_pickledir,"all_accessions.txt"),'w') as handle:
+    handle.write("\n".join(accessions))
