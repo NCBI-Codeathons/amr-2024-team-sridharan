ecdb0b348459d92a1ecc561cf2fe0ec99dcbc5db
Kornelius20-20 <weeraman97@gmail.com>
2024-09-25 22:58:49 -0600

    sorting embeddings

--- src/batch_embeddings.py
+++ src/batch_embeddings.py
@@ -6,32 +6,58 @@ It can also check to see if there are any alrady generated embedding pickles in 
 """
 
 import torch,os, gzip,pickle, argparse
-from esm_embeddings import generate_embeddings
 
-tempfolder = "temp/fastas/"
-embeddingsdir = "temp/embeddings/"
-fastafolder = os.listdir(tempfolder)
 batch_size = 5
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-embeddings = []
 # Parsing command line arguments
-parser = argparse.ArgumentParser(description='Get start and end indices')
+parser = argparse.ArgumentParser(description='Get arguments')
 parser.add_argument('-s', '--start', type=int, default=0, help='Start index (default: 0)')
 parser.add_argument('-e', '--end', type=int, default=1000, help='End index (default: 1000)')
+parser.add_argument('-f', '--fastafol', type=str, default="temp/fastas/", help='Folder of fasta files to generate embeddings for. Expect them to be fa.gz format')
+parser.add_argument('-o', '--outputdir', type=str, default="temp/embeddings/", help='Output directory for embeddings')
+
 args = parser.parse_args()
 
+tempfolder = args.fastafol
+embeddingsdir = args.outputdir
+fastafolder = os.listdir(tempfolder)
 startindex = int(args.start)
 endindex = int(args.end) if int(args.end) < len(fastafolder) else len(fastafolder)
 
 # Load in the embedding model 
 # (Change this to something larger if you have the VRAM to spare)
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model, alphabet = torch.hub.load("facebookresearch/esm:main", "esm2_t12_35M_UR50D")
 model.eval()
 model.to(device)
 
+def generate_embeddings(
+        sequences, # list of tuples of the form ('header','sequence')
+        model,
+        alphabet,
+        embedding_layer=12, # The final layer of the embedding model
+        device=device, # will automatically use CUDA if its available else will fall back on cpu
+):
+
+    batch_converter = alphabet.get_batch_converter()
+    # Generating data batches
+    batch_labels, batch_strs, batch_tokens = batch_converter(sequences)
+    with torch.no_grad():
+        batch_tokens = batch_tokens.to(device)
+        results = model(batch_tokens, repr_layers=[embedding_layer], return_contacts=True)
+    token_representations = results["representations"][embedding_layer].cpu().detach()
+
+    # Generating sequence representating via averaging token representations
+    sequence_representations = []
+    for i, (_, seq) in enumerate(sequences):
+        sequence_representations.append((batch_labels[i],token_representations[i, 1 : len(seq) + 1].mean(0)))
+    
+    del batch_tokens, token_representations, results
+    torch.cuda.empty_cache()
+    return sequence_representations # Embedding dim is 420
 
 
+embeddings = []
 # main loop
 for i in range(startindex,endindex):
     file = fastafolder[i]
@@ -39,16 +65,14 @@ for i in range(startindex,endindex):
     # Create a BytesIO buffer for the gzip file and extract the content
     with gzip.open(os.path.join(tempfolder,file),'rt') as handle:
         data = ''.join(handle.read().split('\n')[1:])
-    i += 1    
 
     try:
         temp = generate_embeddings([(file,data)],model,alphabet)
         embeddings.append( temp )
 
+    # Some proteins have amino acids that the model doesn't recognize. In this case we warn the user and skip that protein
     except KeyError:
         print(f"protein {file} had an unrecognized AA")
-    # Some proteins have amino acids that the model doesn't recognize. In this case we warn the user and skip that protein
-    
 
     # Every 100 batches we dump the currently generated embeddings
     # This is more to prevent the script from eating all your RAM than anything else tbh
--- src/merge_pickles.py
+++ src/merge_pickles.py
@@ -1,7 +1,16 @@
-import os,pickle
+import os,pickle, argparse
 import numpy as np
+import polars as pl
 
-pickledir = "temp/embeddings"
+# Parsing command line arguments
+parser = argparse.ArgumentParser(description='Get arguments')
+parser.add_argument('-ep', '--embeddingspath', type=str, default="temp/embeddings", help='folder where the embedding pickles are')
+parser.add_argument('-d', '--datafile', type=str, default="/shared_venv/data_from_bigquery.csv", help='file with downloaded database in csv format')
+args = parser.parse_args()
+
+pickledir = args.embeddingspath
+alldata = args.datafile
+output_path = alldata.rsplit('/',1)[0]
 
 files = os.listdir(pickledir)
 
@@ -18,10 +27,20 @@ accessions = [i[0][0][:-6] for i in data]
 
 embeddings = np.array(embeddings)
 
-dir_above_pickledir = '/'.join(pickledir.split('/')[:-1])
+dir_above_pickledir = pickledir.rsplit('/',1)[0]
 
 with open(os.path.join(dir_above_pickledir,"all_embeddings.pickle"),'wb') as handle:
     pickle.dump(embeddings,handle)
 
 with open(os.path.join(dir_above_pickledir,"all_accessions.txt"),'w') as handle:
     handle.write("\n".join(accessions))
+
+values = accessions
+
+data = pl.read_csv(alldata)
+# Filter and sort by values in all_embeddings.txt
+data = data.filter(pl.col('protein_acc').is_in(values))
+data = data.with_columns(pl.Series([values.index(x) for x in data['protein_acc']]).alias('sort_index'))
+data = data.sort(by='sort_index')
+
+data.write_csv("/shared_venv/filtered_data.csv")
