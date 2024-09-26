3ec8b3b637708f7ba59168cea7e72dfc386f8db3
Kornelius20-20 <weeraman97@gmail.com>
2024-09-25 21:50:12 -0600

    sorting embeddings

--- src/sort_embeddings.py
+++ src/sort_embeddings.py
@@ -9,6 +9,8 @@ with open(embeddings_txt,'r') as file:
 data = pl.read_csv(alldata)
 
 # Filter and sort by values in all_embeddings.txt
-data = data.filter(pl.col('protein_acc').is_in(values)).sort(by=lambda x: x['protein_acc'].apply(lambda y: values.index(y)))
+data = data.filter(pl.col('protein_acc').is_in(values))
+data = data.with_columns(pl.col('protein_acc').apply(lambda x: values.index(x)).alias('sort_index'))
+data = data.sort(by='sort_index')
 
 data.write_csv("filtered_data.csv")
