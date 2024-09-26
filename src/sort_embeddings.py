b36276e236d258cb3badb9b2f29892f1f0d4a07c
Kornelius20-20 <weeraman97@gmail.com>
2024-09-25 21:46:27 -0600

    embeddings issue

---
+++ src/sort_embeddings.py
@@ -0,0 +1,11 @@
+import polars as pl
+
+alldata = "data_from_bigquery.csv"
+values = "temp/all_embeddings.txt"
+
+data = pl.read_csv(alldata)
+
+# Filter and sort by values in all_embeddings.txt
+data = data.filter(pl.col('protein_acc').is_in(values)).sort(by=lambda x: x['protein_acc'].apply(lambda y: values.index(y)))
+
+data.write_csv("filtered_data.csv")
