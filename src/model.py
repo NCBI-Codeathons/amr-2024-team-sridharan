3e974944635f5ad7c68b62b57a8ce3b4cbd641c8
Ganeshiny Sridharan <ganeshinywork@gmail.com>
2024-09-26 15:15:09 +0530

    self loops in the GATConv layer set to false

--- src/model.py
+++ src/model.py
@@ -18,19 +18,25 @@ class HeteroGraphModel(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels, num_heads=4):
         super().__init__()
 
+        # GraphSAGE for the first layer
         self.conv1 = SAGEConv((-1, -1), hidden_channels)
 
-        self.conv2 = GATConv((-1, -1), hidden_channels, heads=num_heads, concat=False)
+        # GATConv for the second layer, with add_self_loops set to False
+        self.conv2 = GATConv((-1, -1), hidden_channels, heads=num_heads, concat=False, add_self_loops=False)
 
+        # Final linear layer
         self.lin = Linear(hidden_channels, out_channels)
 
     def forward(self, x, edge_index):
+        # First layer: GraphSAGE
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         
+        # Second layer: GATConv
         x = self.conv2(x, edge_index)
         x = F.relu(x)
         
+        # Final linear layer
         x = self.lin(x)
         return x
 
@@ -40,12 +46,14 @@ class LinkPredictor(torch.nn.Module):
         self.lin = torch.nn.Linear(in_channels, 1)
 
     def forward(self, z_protein, z_class, edge_label_index):
+        # Extract the embeddings for source and target nodes
         src_emb = z_protein[edge_label_index[0]]  
         tgt_emb = z_class[edge_label_index[1]]    
 
         # Element-wise product of source and target node embeddings
         edge_features = src_emb * tgt_emb
 
+        # Predict edge probability using a sigmoid layer
         return torch.sigmoid(self.lin(edge_features)).squeeze()
 
 class HeteroLinkPredictionModel(torch.nn.Module):
@@ -61,8 +69,10 @@ class HeteroLinkPredictionModel(torch.nn.Module):
         self.link_predictor = LinkPredictor(out_channels)
 
     def forward(self, data):
+        # Get node embeddings from the GNN
         z_dict = self.gnn(data.x_dict, data.edge_index_dict)
 
+        # Use the link predictor to predict the existence of edges
         pred = self.link_predictor(
             z_dict['protein'], z_dict['class'],
             data['protein', 'interacts_with', 'drug_class'].edge_label_index
