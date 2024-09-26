37c73e0aa9f6a127fbe0e1521677b66ffec9b129
Kornelius20-20 <weeraman97@gmail.com>
2024-09-25 22:12:47 -0600

    sorting embeddings

--- src/sort_embeddings.py
+++ src/sort_embeddings.py
@@ -1,16 +1,15 @@
 import polars as pl
 
-alldata = "/shared_venv/data_from_bigquery.csv"
+alldata = "data_from_bigquery.csv"
 embeddings_txt = "temp/all_accessions.txt"
 
 with open(embeddings_txt,'r') as file:
-    values = file.readlines()
+    values = [i.strip() for i in file.readlines()]
 
 data = pl.read_csv(alldata)
-
 # Filter and sort by values in all_embeddings.txt
 data = data.filter(pl.col('protein_acc').is_in(values))
-data = data.with_columns(pl.col('protein_acc').apply(lambda x: values.index(x)).alias('sort_index'))
+data = data.with_columns(pl.Series([values.index(x) for x in data['protein_acc']]).alias('sort_index'))
 data = data.sort(by='sort_index')
 
 data.write_csv("filtered_data.csv")
